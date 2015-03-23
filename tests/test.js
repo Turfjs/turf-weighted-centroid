c429a41f2a0723711fdff8dfccf2998e7b18450b
Corey White <smortopahri@gmail.com>
2015-03-22 23:59:19 -0400

    Added package.json and tape for testing

---
+++ tests/test.js
@@ -0,0 +1,11 @@
+var meanCenter = require('../'),
+    test = require('tape'),
+    fs = require('fs');
+
+test('mean center', function (t){
+
+});
+
+test('weighted mean center', function (t){
+
+});
